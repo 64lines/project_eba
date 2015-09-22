e742a0070f76a8047b454e1b41c833830e79cd16
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2015-09-22 14:12:41 -0500

    Include login template and logic.

---
+++ eba/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+USERNAME_LABEL = 'Nombre de usuario'
+PASSWORD_LABEL = 'Clave'
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label=USERNAME_LABEL, required=True)
+    password = forms.CharField(label=PASSWORD_LABEL, widget=forms.PasswordInput)
--- eba/views.py
+++ eba/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext, loader
+from forms import LoginForm
 
 def index_page(request): 
     app_name = 'EBA'
+    
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST) 
+    else:
+        login_form = LoginForm()
+    
+    if login_form and login_form.is_valid():
+        username = login_form['username']
+        password = login_form['password']
+
     variables = RequestContext(request, {
+        'login_form': login_form,
         'app_name': app_name 
     })
     return render_to_response('index.html', variables)
