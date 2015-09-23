b68c77f422e3a0af1d69787a1d2ef564a08f48e8
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2015-09-22 23:46:33 -0500

    Some additional changes related to the form

--- eba/forms.py
+++ eba/forms.py
@@ -5,4 +5,4 @@ PASSWORD_LABEL = 'Clave'
 
 class LoginForm(forms.Form):
     username = forms.CharField(label=USERNAME_LABEL, required=True)
-    password = forms.CharField(label=PASSWORD_LABEL, widget=forms.PasswordInput)
+    password = forms.CharField(label=PASSWORD_LABEL, widget=forms.PasswordInput, required=True)
